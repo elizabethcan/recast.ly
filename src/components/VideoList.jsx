a4c6807437874ed3a448973cb0ce96496b5f0832
elizabethcan <elizabeth230@gmail.com>
2019-01-16 16:44:07 -0800

    Refactor videoList and videoPlayer to conform to specs

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,14 +1,16 @@
 import VideoListEntry from './VideoListEntry.js';
 
-var VideoList = (props) => (
-  <div className="video-list">
-    <VideoListEntry video = {props.videos[0]}/>
-    <VideoListEntry video = {props.videos[1]}/>
-    <VideoListEntry video = {props.videos[2]}/>
-    <VideoListEntry video = {props.videos[3]}/>
-    <VideoListEntry video = {props.videos[4]}/>
-  </div>
-);
+var VideoList = (props) => {
+  var listOfVideos = [];
+  for (var i =0; i < props.videos.length; i++) {
+    listOfVideos.push(<VideoListEntry key = {i} video = {props.videos[i]}/>);
+  }
+  return (
+    <div className="video-list">
+      {listOfVideos}
+    </div>
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,22 +1,14 @@
-class VideoPlayer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {currentUrl: `https://www.youtube.com/embed/${this.props.video.id.videoId}`, currentVideo: this.props.video};
-  }
-  render() {
-    return (
-      <div className="video-player">
-        <div className="embed-responsive embed-responsive-16by9">
-          <iframe className="embed-responsive-item" src={this.state.currentUrl} allowFullScreen></iframe>
-        </div>
-        <div className="video-player-details">
-          <h3>{this.state.currentVideo.snippet.title}</h3>
-          <div>{this.state.currentVideo.snippet.description}</div>
-        </div>
-      </div>
-    );
-  }
-}
+var VideoPlayer = (props) => (
+  <div className="video-player">
+    <div className="embed-responsive embed-responsive-16by9">
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen></iframe>
+    </div>
+    <div className="video-player-details">
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
+    </div>
+  </div>
+);
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
