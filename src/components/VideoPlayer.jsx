ee61cf96ea72c63068c4f6cb6066109d91957361
elizabethcan <elizabeth230@gmail.com>
2019-01-16 16:14:53 -0800

    Implement videoList and videoPlayer
    VideoList and VideoPlayer currently running off of test data. Still need to implement dynamic search off of the Youtube API.

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,14 +1,22 @@
-var VideoPlayer = () => (
-  <div className="video-player">
-    <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
-    </div>
-    <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
-    </div>
-  </div>
-);
+class VideoPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {currentUrl: `https://www.youtube.com/embed/${this.props.video.id.videoId}`, currentVideo: this.props.video};
+  }
+  render() {
+    return (
+      <div className="video-player">
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src={this.state.currentUrl} allowFullScreen></iframe>
+        </div>
+        <div className="video-player-details">
+          <h3>{this.state.currentVideo.snippet.title}</h3>
+          <div>{this.state.currentVideo.snippet.description}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
